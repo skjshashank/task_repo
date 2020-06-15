b12c387f6b2f6527ecf311290bed32b5568de51c
skjshashank <45492933+skjshashank@users.noreply.github.com>
2020-06-15 11:59:12 +0530

    Update index.php
    first commit

--- index.php
+++ index.php
@@ -1,1 +1,19 @@
-welcome to task project..
+<body bgcolor=#f4f7c5>
+ <h1>WELCOME TO THIS TASK   </h1>
+
+ 
+<pre>
+
+<?php 
+
+ 
+
+print `ifconfig`;
+
+print "\n ";
+
+print `free -m`;
+
+?>
+
+</pre>
